5229ce719954997ecbec863ea75319e4f24329c4
Amir Salehi <salehi.amir9@gmail.com>
2022-11-22 17:10:07 +0330

    no border

--- pages/sign.jsx
+++ pages/sign.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Layout from "src/components/Layout";
 import Form from "src/components/Sign/Form";
-import FormImageDesktop from "src/images/Form-1.png";
+// import FormImageDesktop from "src/images/Form-1.png";
 import FormImageMobile from "src/images/Form-3.png";
 
 export default function SignUpPage() {
   return (
-    <Layout title="ورود / ثبت نام">
+    <Layout title="ورود یا ثبت نام">
       <div className="flex flex-col-reverse md:flex-row my-2">
         <Form />
         <div className="w-full md:w-1/2 relative h-[400px] md:h-screen">
