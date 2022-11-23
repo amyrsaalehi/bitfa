b6c5d22954f9d8d2463863ccf29114589b23d1a9
Amir Salehi <salehi.amir9@gmail.com>
2022-11-23 13:35:03 +0330

    modal text

--- src/components/Shared/ModalText.jsx
+++ src/components/Shared/ModalText.jsx
@@ -1,89 +1,87 @@
 import { forwardRef, useId } from "react";
 
-const ModalText = forwardRef(
-  (
-    {
-      btn,
-      btnType = "btn-ghost",
-      modal,
-      nobg = false,
-      noclose = false,
-      nomobile = false,
-      clickaway = true,
-      top = 0,
-      btnClass = "",
-      modalClass = "",
-    },
-    ref
-  ) => {
-    const id = useId();
+function ModalText(
+  {
+    btn,
+    btnType = "btn-ghost",
+    modal,
+    nobg = false,
+    noclose = false,
+    nomobile = false,
+    clickaway = true,
+    top = 0,
+    btnClass = "",
+    modalClass = "",
+  },
+  ref
+) {
+  const id = useId();
 
-    return (
-      <>
+  return (
+    <>
+      <label
+        htmlFor={id}
+        className={`btn ${btnType} ${btnClass} normal-case`}
+        ref={ref}
+      >
+        {btn}
+      </label>
+
+      <input type="checkbox" id={id} className="modal-toggle" />
+      {!clickaway ? (
+        <div
+          className={`modal ${
+            !nomobile && "modal-bottom sm:modal-middle"
+          } ${modalClass}`}
+          style={{
+            top,
+          }}
+        >
+          <div
+            className={`modal-box sm:max-h-[80vh] ${
+              nobg && "bg-transparent p-0"
+            }`}
+          >
+            {!noclose && (
+              <label
+                htmlFor={id}
+                className="btn btn-sm btn-circle absolute right-2 top-2"
+              >
+                ✕
+              </label>
+            )}
+            {modal}
+          </div>
+        </div>
+      ) : (
         <label
           htmlFor={id}
-          className={`btn ${btnType} ${btnClass} normal-case`}
-          ref={ref}
+          className={`modal ${
+            !nomobile && "modal-bottom sm:modal-middle"
+          } ${modalClass}`}
+          style={{
+            top,
+          }}
         >
-          {btn}
-        </label>
-
-        <input type="checkbox" id={id} className="modal-toggle" />
-        {!clickaway ? (
           <div
-            className={`modal ${
-              !nomobile && "modal-bottom sm:modal-middle"
-            } ${modalClass}`}
-            style={{
-              top,
-            }}
+            className={`modal-box sm:max-h-[80vh] ${
+              nobg && "bg-transparent p-0"
+            }`}
           >
-            <div
-              className={`modal-box sm:max-h-[80vh] ${
-                nobg && "bg-transparent p-0"
-              }`}
-            >
-              {!noclose && (
-                <label
-                  htmlFor={id}
-                  className="btn btn-sm btn-circle absolute right-2 top-2"
-                >
-                  ✕
-                </label>
-              )}
-              {modal}
-            </div>
+            {!noclose && (
+              <label
+                htmlFor={id}
+                className="btn btn-sm btn-circle absolute right-2 top-2"
+              >
+                ✕
+              </label>
+            )}
+            {modal}
           </div>
-        ) : (
-          <label
-            htmlFor={id}
-            className={`modal ${
-              !nomobile && "modal-bottom sm:modal-middle"
-            } ${modalClass}`}
-            style={{
-              top,
-            }}
-          >
-            <div
-              className={`modal-box sm:max-h-[80vh] ${
-                nobg && "bg-transparent p-0"
-              }`}
-            >
-              {!noclose && (
-                <label
-                  htmlFor={id}
-                  className="btn btn-sm btn-circle absolute right-2 top-2"
-                >
-                  ✕
-                </label>
-              )}
-              {modal}
-            </div>
-          </label>
-        )}
-      </>
-    );
-  }
-);
+        </label>
+      )}
+    </>
+  );
+}
 
-export default ModalText;
+export default forwardRef(ModalText);
